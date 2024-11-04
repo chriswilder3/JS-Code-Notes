62b9694bf7af9160b1ffd043489c525990319b88
Sachin Sekiro <chriswilderdelta19@gmail.com>
2024-11-05 00:49:41 +0530

    Update objects3_json.js

--- objects3_json.js
+++ objects3_json.js
@@ -1,5 +1,29 @@
 // Objects destructure and JSON
 
     const course = {
-        name: 'django'
+        name: 'django',
+        courseInstructor: 'kailey',
+        year : 2021
     }
+    // console.log( course.courseInstructor);
+    // Now while accessing its hassle to write as below everytime
+    // Destructuring allows us to break the objects into its parts
+    // We need to give all keys or the keys we need inside {}
+
+    const { courseInstructor, year} = course;
+    // Now it has broken down RHS into LHS, but there are only 2 keys which 
+    // these broken down and other are ignored
+
+    console.log(courseInstructor); // kailey
+    console.log(year); // 2021
+
+    // We can rename the destructure items to give easier names
+    const {courseInstructor: teacher} = course;
+    console.log(teacher); // kailey
+
+// JSON are similar to object exept : their keys are strictly strings
+    // ie. (need double quotes)
+    // and their values allows basic types like strings, numbers, booleans 
+    // and arrays. JSON values CAN NOT be : functions, comments, NaN, Null etc
+
+
